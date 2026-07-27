09c6246e8b306c4a89c8e934b85ac6b65514dcbf
Asfahan <asfahans@gmail.com>
2026-07-27 14:52:14 +0530

    Add backend PHP scripts for feedback processing, login auth, and status updates

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// database configuration
+define('DB_HOST', '216.10.241.228');
+define('DB_USER', 'webwokam_test');
+define('DB_PASS', '@1TesT1@');
+define('DB_NAME', 'webwokam_mrvrmshare');
+
+// Define your base URL or directory path
+define('BASE_URL', 'https://www.mrvrmshare.com/');
---
+++ email_config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'host' => 'smtp.gmail.com',
+    'port' => 587,
+    'username' => 'info@vrmshares.com',
+    'password' => 'ikzddscopnlnlfsh',
+    'from_email' => 'info@vrmshares.com',
+    'from_name' => 'Yogesh',
+    'admin_email' => 'info@vrmshares.com',
+];
+?>
